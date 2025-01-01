a6c99878a853fd49e96f8c07b2bb4a70148141ed
iMaaNor <imaanor@protonmail.com>
2025-01-01 22:38:53 +0330

    updated.

--- local/.local/scripts/kbdlight.sh
+++ local/.local/scripts/kbdlight.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-STATE=$(brightnessctl --device='input4::scrolllock' g)
+STATE=$(brightnessctl --device='input3::scrolllock' g)
 
 if [ "$STATE" -eq 1 ]; then
-	brightnessctl --device='input4::scrolllock' set 0 && notify-send -a "Control Center" "Keyboard light turned on"
+	brightnessctl --device='input3::scrolllock' set 0 && notify-send -a "Control Center" "Keyboard light turned on"
 else
-	brightnessctl --device='input4::scrolllock' set 1 && notify-send -a "Control Center" "Keyboard light turned off"
+	brightnessctl --device='input3::scrolllock' set 1 && notify-send -a "Control Center" "Keyboard light turned off"
 fi
--- local/.local/scripts/n_p_workspace.sh
+++ local/.local/scripts/n_p_workspace.sh
@@ -11,10 +11,16 @@ command=$1
 
 current=$(awk 'NR==1' <(hyprctl activeworkspace) | awk '{print $3}')
 
-if [ $command = '+' ]; then
+if [ $command = 'n' ]; then
     ws=$(clamp 1 6 $(($current+1)))
     hyprctl dispatch workspace $ws
-else
+elif [ $command = "p" ]; then
     ws=$(clamp 1 6 $(($current-1)))
     hyprctl dispatch workspace $ws
+elif [ $command = "mn" ]; then
+    ws=$(clamp 1 6 $(($current+1)))
+    hyprctl dispatch movetoworkspace $ws
+elif [ $command = "mp" ]; then
+    ws=$(clamp 1 6 $(($current-1)))
+    hyprctl dispatch movetoworkspace $ws
 fi
